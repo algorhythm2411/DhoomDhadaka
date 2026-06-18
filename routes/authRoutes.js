69fedf671b5b9e46cf62250318aed8cc585c1022
algorhythm2411 <163877489+algorhythm2411@users.noreply.github.com>
2026-06-18 12:44:00 +0530

    Add authentication routes for user registration and login

---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { registerUser, loginUser } = require('../controllers/authController');
+const router = express.Router();
+
+// Routes definition
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
